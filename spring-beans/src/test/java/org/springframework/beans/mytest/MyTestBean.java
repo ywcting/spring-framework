11505023b6d5c71d01d1314adee10099f40c4efa
cting <ywcting@126.com>
2020-07-07 08:06:44 +0800

    bean测试

---
+++ spring-beans/src/test/java/org/springframework/beans/mytest/MyTestBean.java
@@ -0,0 +1,13 @@
+package org.springframework.beans.mytest;
+
+public class MyTestBean {
+	private String testStr = "myTestSr";
+
+	public String getTestStr() {
+		return testStr;
+	}
+
+	public void setTestStr(String testStr) {
+		this.testStr = testStr;
+	}
+}
