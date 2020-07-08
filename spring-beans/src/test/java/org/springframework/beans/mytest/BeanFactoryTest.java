13ffb3bf8be89567c345905c72d092301e1b663d
cting <ywcting@126.com>
2020-07-09 01:09:52 +0800

    bean测试

---
+++ spring-beans/src/test/java/org/springframework/beans/mytest/BeanFactoryTest.java
@@ -0,0 +1,15 @@
+package org.springframework.beans.mytest;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+@SuppressWarnings("deprecation")
+public class BeanFactoryTest {
+	@Test
+	public void testSimpleLoad(){
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("org/springframework/beans/factory/BeanFactoryTest.xml"));
+		MyTestBean bean = (MyTestBean)bf.getBean("myTestBean");
+		System.out.println(bean.getTestStr());
+	}
+}
---
+++ spring-beans/src/test/java/org/springframework/beans/mytest/MyTestBean.java
@@ -0,0 +1,13 @@
+package org.springframework.beans.mytest;
+
+public class MyTestBean {
+	private String testStr = "testStr";
+
+	public String getTestStr() {
+		return testStr;
+	}
+
+	public void setTestStr(String testStr) {
+		this.testStr = testStr;
+	}
+}
